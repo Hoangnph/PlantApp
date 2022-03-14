feac67ae15ee181ed79c0923abf82439bfdd831a
Hoangnph <35054368+Hoangnph@users.noreply.github.com>
2022-03-14 09:20:40 +0700

    Finish Setup

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+  // Add custom Font
+  let customFonts = {
+    "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
+    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
+  };
+  
+
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
