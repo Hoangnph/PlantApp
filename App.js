192a99b05cfc588ac10bc40035c42bf7d2422b9c
Hoangnph <35054368+Hoangnph@users.noreply.github.com>
2022-03-15 11:38:00 +0700

    Finish PlatApp small design

--- App.js
+++ App.js
@@ -1,25 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+// Screens
+import {  PlantDetail } from './src/screens'
+// Tab
+import Tabs from './src/navigation/tabs'
 
-
-  // Add custom Font
-  let customFonts = {
-    "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
-    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
-  };
-  
-
+const Stack = createStackNavigator();
 
 export default function App() {
 
-
+  const [loaded] = useFonts({
+    "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
+    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
+    "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator
+        screenOptions={{ 
+          headerShown : false
+        }}
+        initialRouteName={"Home"}
+      >
+
+        <Stack.Screen 
+        name= "Home"
+        component={ Tabs }
+        options={{headerShown: false}}
+        />
+
+        {/* Screen */}
+        <Stack.Screen 
+          name= "PlantDetail"
+          component={ PlantDetail }
+          options={{headerShown: false}}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
